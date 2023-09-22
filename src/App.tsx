075d73bd8fe00b11cb6b81b49ce31717240cab90
isaias <isaias.chavez.secure@gmail.com>
2023-09-22 12:05:34 -0600

    creando tarjetas

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DataSheet from './components/DataSheet';
+
+function App() {
+  return (
+    <div>
+      <DataSheet/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DataSheet.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const DataSheet = () => {
+  return (
+    <div className='border  p-4'>
+      <h1>Nombre lugar</h1>
+      <h2>una descripción un poco más compleja</h2>
+      <h3>Precio promedio   <h2>200</h2></h3>
+      <h3>Calificación en estrellas</h3>
+      <h2>Comentarios</h2>
+        <h3>numero <a type='phone'> 9511212233</a></h3> 
+        <button>ver ubicación</button>
+    </div>
+  )
+}
+
+export default DataSheet
